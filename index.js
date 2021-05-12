f4f2b2d3042ccefdce37828a7170e729da5c637b
YanJobs <yan@yanjobs.me>
2021-05-12 16:21:29 +0000

    :broom: Clean 🧼 (prettier)

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import nanoexpress from 'nanoexpress';
-import fs from 'fs';
+import nanoexpress from "nanoexpress";
+import fs from "fs";
 
-let rawdata = fs.readFileSync('links.json');
+let rawdata = fs.readFileSync("links.json");
 let urls = JSON.parse(rawdata);
 const app = nanoexpress();
 
-app.get('/', async (req, res) => {
-	return res.redirect("https://www.yanjobs.me")
-	
+app.get("/", async (req, res) => {
+  return res.redirect("https://www.yanjobs.me");
 });
 
-app.get('/:id', async (req, res) => {
-    let url = urls[req.params.id];
-    return res.redirect(url);
+app.get("/:id", async (req, res) => {
+  let url = urls[req.params.id];
+  return res.redirect(url);
 });
 app.listen(3000);
